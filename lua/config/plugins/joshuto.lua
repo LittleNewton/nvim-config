484a224e478217cc5c15eef8c78d7905190a42df
Peng Liu <littlenewton6@gmail.com>
2023-09-27 22:51:23 +0800

    [+] Add `joshuto` plugin profiles.

--- lua/config/plugins/joshuto.lua
+++ lua/config/plugins/joshuto.lua
@@ -1,13 +1,15 @@
+-- Author: Peng Liu
+-- Create Date: 27 Sept. 2023
+-- Update Date: 27 Sept. 2023
+
+
+
 return {
-	"theniceboy/joshuto.nvim",
-	very_lazy = true,
-	cmd = "Joshuto",
-	config = function()
-		-- let g:joshuto_floating_window_winblend = 0
-		-- let g:joshuto_floating_window_scaling_factor = 1.0
-		-- let g:joshuto_use_neovim_remote = 1 " for neovim-remote support
-		vim.g.joshuto_floating_window_scaling_factor = 1.0
-		vim.g.joshuto_use_neovim_remote = 1
-		vim.g.joshuto_floating_window_winblend = 0
-	end
+    "theniceboy/joshuto.nvim",
+    cmd = "Joshuto",
+    config = function()
+        vim.g.joshuto_floating_window_scaling_factor = 1.0
+        vim.g.joshuto_use_neovim_remote = 1
+        vim.g.joshuto_floating_window_winblend = 0
+    end
 }
