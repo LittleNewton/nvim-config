a19698f04dfd4c2dd23e0a11ff97118b115d4ee1
Peng Liu <littlenewton6@gmail.com>
2023-11-04 02:16:15 +0800

    [*] Update mason plugin's config.

--- lua/config/plugins/mason.lua
+++ lua/config/plugins/mason.lua
@@ -1,22 +1,28 @@
+-- Author: Peng Liu
+-- Create Date: 27 Sept. 2023
+-- Update Date: 27 Sept. 2023
+
+
+
 return {
-	"williamboman/mason-lspconfig.nvim",
-	lazy = false,
-	dependencies = {
-		{ "williamboman/mason.nvim", build = ":MasonUpdate", },
-	},
-	config = function()
-		require("mason").setup({
-			ui = {
-				icons = {
-					package_installed = "✓",
-					package_pending = "➜",
-					package_uninstalled = "✗"
-				}
-			}
-		})
-		require("mason-lspconfig").setup({
-			ensure_installed = { "lua_ls", },
-			automatic_installation = true,
-		})
-	end
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    dependencies = {
+        { "williamboman/mason.nvim", build = ":MasonUpdate", },
+    },
+    config = function()
+        require("mason").setup({
+            ui = {
+                icons = {
+                    package_installed = "✓",
+                    package_pending = "➜",
+                    package_uninstalled = "✗"
+                }
+            }
+        })
+        require("mason-lspconfig").setup({
+            ensure_installed = { "lua_ls", },
+            automatic_installation = true,
+        })
+    end
 }
