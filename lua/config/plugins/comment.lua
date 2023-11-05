589565d4ae447d3c37918a242ae9bb444bfe7d9f
Peng Liu <littlenewton6@gmail.com>
2023-11-05 10:21:05 +0800

    [+] A perfect comment plugins profile.
    我理解了里面全部的含义。

--- lua/config/plugins/comment.lua
+++ lua/config/plugins/comment.lua
@@ -1,29 +1,48 @@
+-- Author: Peng Liu
+-- Email: littlenewton6@gmail.com
+-- Create Date: 05 Nov. 2023
+-- Update Date: 05 Nov. 2023
+
+
+
 return {
-	"numToStr/Comment.nvim",
-	config = function()
-		local api = require("Comment.api")
-		vim.keymap.set("n", "<leader>cn", api.locked("comment.linewise.current"))
-		vim.keymap.set("n", "<leader>cu", api.locked("uncomment.linewise.current"))
-		local esc = vim.api.nvim_replace_termcodes('<ESC>', true, false, true)
-		vim.keymap.set('x', '<leader>cn', function()
-			vim.api.nvim_feedkeys(esc, 'nx', false)
-			api.locked('comment.linewise')(vim.fn.visualmode())
-		end)
-		vim.keymap.set('x', '<leader>cu', function()
-			vim.api.nvim_feedkeys(esc, 'nx', false)
-			api.locked('uncomment.linewise')(vim.fn.visualmode())
-		end)
-		vim.keymap.set('x', '<leader>ce', function()
-			vim.api.nvim_feedkeys(esc, 'nx', false)
-			api.locked('toggle.blockwise')(vim.fn.visualmode())
-		end)
-		require('Comment').setup({
-			padding = true,
-			sticky = true,
-			mappings = {
-				basic = false,
-				extra = false,
-			},
-		})
-	end
+    "numToStr/Comment.nvim",
+    config = function()
+        local api = require("Comment.api")
+
+        -- Set keybindings for normal mode.
+        vim.keymap.set("n", "<leader>nn", api.locked("comment.linewise.current"))
+        vim.keymap.set("n", "<leader>uu", api.locked("uncomment.linewise.current"))
+        vim.keymap.set("n", "<leader>cc", api.locked("toggle.blockwise.current"))
+
+        -- Set keybindings for visual block mode.
+        local esc = vim.api.nvim_replace_termcodes('<ESC>', true, false, true)
+        vim.keymap.set('x', '<leader>nn', function()
+            vim.api.nvim_feedkeys(esc, 'nx', false)
+            api.locked('comment.linewise')(vim.fn.visualmode())
+        end)
+        vim.keymap.set('x', '<leader>uu', function()
+            vim.api.nvim_feedkeys(esc, 'nx', false)
+            api.locked('uncomment.linewise')(vim.fn.visualmode())
+        end)
+        vim.keymap.set('x', '<leader>cc', function()
+            vim.api.nvim_feedkeys(esc, 'nx', false)
+            api.locked('toggle.blockwise')(vim.fn.visualmode())
+        end)
+
+        -- padding.
+        require('Comment').setup({
+            -- Add padding between comment contents and comment delimiters.
+            padding = true,
+
+            -- Keep indention when adding comment.
+            sticky = true,
+
+            -- Disable default keybindings.
+            mappings = {
+                basic = false,
+                extra = false,
+            },
+        })
+    end
 }
