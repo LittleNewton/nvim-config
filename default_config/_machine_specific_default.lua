f6fb631f3771a386dd7cebfff4011cf860e349cc
Peng Liu <littlenewton6@gmail.com>
2023-11-05 09:45:19 +0800

    [*] Update snips_author.

--- default_config/_machine_specific_default.lua
+++ default_config/_machine_specific_default.lua
@@ -1,6 +1,6 @@
 -- please read through this file and change what you need to change
 
-vim.g.snips_author = 'David Chen'
+vim.g.snips_author = 'Peng Liu'
 
 vim.g.python3_host_prog = '/usr/bin/python3'
 
