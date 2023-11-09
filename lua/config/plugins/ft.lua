9c33579d30ac4d3595b87a7c893b3b7409943a1c
Peng Liu <littlenewton6@gmail.com>
2023-11-09 21:24:59 +0800

    [+] Add format config.

--- lua/config/plugins/ft.lua
+++ lua/config/plugins/ft.lua
@@ -1,10 +1,17 @@
+-- Author: Peng Liu
+-- Email: littlenewton6@gmail.com
+-- Create Date: 09 Nov. 2023
+-- Update Date: 09 Nov. 2023
+
+
+
 return {
-	{
-		"dart-lang/dart-vim-plugin",
-		ft = "dart",
-		config = function()
-			vim.g.dart_corelib_highlight = false
-			vim.g.dart_format_on_save = false
-		end
-	}
+    {
+        "dart-lang/dart-vim-plugin",
+        ft = "dart",
+        config = function()
+            vim.g.dart_corelib_highlight = false
+            vim.g.dart_format_on_save = false
+        end
+    }
 }
